f9d3cf00a7f8fa21d0063b29a43988709d293f9e
earthpywt <piyawat.kus@gmail.com>
2023-10-19 13:54:29 +0700

    assignment 10

--- vaccine-book-app/src/components/Banner.tsx
+++ vaccine-book-app/src/components/Banner.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from "./banner.module.css";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
 
 export default function Banner() {
     const covers = [
@@ -13,6 +14,8 @@ export default function Banner() {
     ];
     const [index, setIndex] = useState(0);
     const router = useRouter();
+    const { data: session } = useSession();
+
     return (
         <div className={styles.banner} onClick={() => setIndex(index + 1)}>
             <Image
@@ -28,6 +31,13 @@ export default function Banner() {
                     Protecting Health, Empowering Communities
                 </h3>
             </div>
+
+            {session ? (
+                <div className="z-30 absolute top-5 right-10 font-semibold text-cyan-800 text-xl">
+                    Hello {session.user?.name}
+                </div>
+            ) : null}
+
             <button
                 className="bg-white text-cyan-600 border border-cyan-600 font-semibold py-2 px-2 m-5 rounded z-30 absolute bottom-0 right-0 hover:bg-cyan-600 hover:text-white hover:border-transparent"
                 onClick={(e) => {
--- vaccine-book-app/src/components/TopMenu.tsx
+++ vaccine-book-app/src/components/TopMenu.tsx
@@ -1,14 +1,39 @@
 // import styles from './topmenu.module.css'
-import Image from 'next/image';
-import TopMenuItem from './TopMenuItem';
+import Image from "next/image";
+import TopMenuItem from "./TopMenuItem";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { Link } from "@mui/material";
 
-export default function TopMenu () {
+export default async function TopMenu() {
+    const session = await getServerSession(authOptions);
     return (
-        <div className='h-[50px] bg-white fixed top-0 inset-x-0 z-30 border-y-1 border-gray-400 border-solid flex flex-row justify-end'>
-            <TopMenuItem title='Booking' pageRef='/booking'/>
-            <Image src={'/img/medical-5459653_640.png'} className='h-full w-auto' alt='logo' width={0} height={0} sizes='100vh'/>
-            
-            
+        <div className="h-[50px] bg-white fixed top-0 inset-x-0 z-30 border-y-1 border-gray-400 border-solid flex flex-row justify-end">
+            <div>
+                {session ? (
+                    <Link href="/api/auth/signout">
+                        <div className="flex items-center absolute left-0 h-full px-2 text-cyan-600 text-sm">
+                            Sign-Out of {session.user?.name}
+                        </div>
+                    </Link>
+                ) : (
+                    <Link href="/api/auth/signin">
+                        <div className="flex items-center absolute left-0 h-full px-2 text-cyan-600 text-sm">
+                            Sign-In
+                        </div>
+                    </Link>
+                )}
+            </div>
+
+            <TopMenuItem title="Booking" pageRef="/booking" />
+            <Image
+                src={"/img/medical-5459653_640.png"}
+                className="h-full w-auto"
+                alt="logo"
+                width={0}
+                height={0}
+                sizes="100vh"
+            />
         </div>
     );
 }
